c921e5e4e218bfe83a77dedf472daf31d38b3f33
Jesse De Jong <jdoceau@gmail.com>
2021-06-26 13:57:42 +1000

    Built function to identify each segment by past/current/future

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,6 +11,31 @@ var threePM = document.getElementById("1500");
 var fourPM = document.getElementById("1600");
 var fivePM = document.getElementById("1700");
 
+var timeslots = [
+    nineAM,
+    tenAM,
+    elevenAM,
+    twelvePM,
+    onePM,
+    twoPM,
+    threePM,
+    fourPM,
+    fivePM
+];
+
+var ts = [
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17"
+];
+
+
 function getDate () { 
     var currentDate = moment().format("dddd, MMMM Do");
 
@@ -19,10 +44,25 @@ function getDate () {
 
 
 
+function colorSegments () {
+    var currentTime = moment().format("HH");
+
+    for ( i = 0; i < ts.length; i++ ) {
+        if (currentTime === ts[i]) {                                    // For the current hour
+            timeslots[i].setAttribute("style", "background: purple");   // Alter styling
+        } else if (currentTime < ts[i]) {                               // For future hours
+            timeslots[i].setAttribute("style", "background: green");    // Alter styling
+        } else if (currentTime > ts[i]) {                               // For past hours
+            timeslots[i].setAttribute("style", "background: red");      // Alter styling
+    }
+}
+}
+
 
 
 function init () {
     getDate();
+    colorSegments();
 }
 
 
