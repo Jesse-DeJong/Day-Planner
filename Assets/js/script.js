73cb4da77d9a772b8de530f45fe948c445038e02
Jesse De Jong <jdoceau@gmail.com>
2021-06-28 22:31:16 +1000

    Built save and local storage functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -36,10 +36,42 @@ var ts = [
 ];
 
 
-function getDate () { 
-    var currentDate = moment().format("dddd, MMMM Do");
 
-    dateEl.innerHTML = currentDate;
+function saveData (event) {
+    event.preventDefault();
+
+    var hour = event.target.dataset.index;  // Gets the data-index value from which save was pressed
+    console.log(hour);
+
+    var tsIndex = ts.findIndex(             // Get the index of [ts] of the hour to be saved 
+        (time, index) => time == hour       // Roughly equal as hour is a number not a string
+    );
+    console.log(tsIndex);
+    content = timeslots[tsIndex];           // Using the [ts] index access the DOM through [timeSlots]
+    console.log(content);
+
+    string = content.value;                 // Get the text value of user input in the targeted hour
+    console.log(string);
+
+    stringJSON = JSON.stringify(string);    // JSON-ify the string to preserve how it was entered into the field
+    console.log(stringJSON);
+    
+    localStorage.setItem(hour, stringJSON); // Store the users inputs in local storage
+}
+
+function getLocalStorage () {
+    for (i = 0; i < ts.length; i++) {                       // Loop over all hours for if they have stored data
+        if (localStorage.getItem(ts[i]) !== null) {         // Skip if there isnt a stored value
+            var j = localStorage.getItem(ts[i]);                // Download stored value
+            timeslots[i].textContent = j;                       // Set the parsed results as the text content of their respective hour
+    }
+  }
+}
+
+function getDate () { 
+    var currentDate = moment().format("dddd, MMMM Do");     // Generate the current date through moment.js API
+                                                            // Format date as eg Monday, June 10th
+    dateEl.innerHTML = currentDate;                         // Inject formatted date to DOM
 }
 
 
@@ -49,19 +81,20 @@ function colorSegments () {
 
     for ( i = 0; i < ts.length; i++ ) {
         if (currentTime === ts[i]) {                                    // For the current hour
-            timeslots[i].setAttribute("style", "background: purple");   // Alter styling
+            timeslots[i].setAttribute("style", "background: var(--purple)");   // Alter styling
         } else if (currentTime < ts[i]) {                               // For future hours
-            timeslots[i].setAttribute("style", "background: green");    // Alter styling
+            timeslots[i].setAttribute("style", "background: var(--green)");    // Alter styling
         } else if (currentTime > ts[i]) {                               // For past hours
-            timeslots[i].setAttribute("style", "background: red");      // Alter styling
+            timeslots[i].setAttribute("style", "background: var(--red)");      // Alter styling
     }
-}
+  }
 }
 
 
 
 function init () {
     getDate();
+    getLocalStorage();
     colorSegments();
 }
 
